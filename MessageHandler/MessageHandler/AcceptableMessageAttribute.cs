066fb484f97c9b8a3e8e6ceed6c32a0f1a0ffb92
pinocch10 <den_85@tut.by>
2014-01-09 18:42:58 +0200

    Added: MessageHandler project

---
+++ MessageHandler/MessageHandler/AcceptableMessageAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageHandler
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    class AcceptableMessageAttribute : Attribute
+    {
+        public Type MessageType { get; set; }
+    }
+}
---
+++ MessageHandler/MessageHandler/Handler/Handler1.cs
@@ -0,0 +1,18 @@
+﻿using MessageHandler.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageHandler.Handler
+{
+    [AcceptableMessage(MessageType = typeof(Message1))]
+    class Handler1 : HandlerBase
+    {
+        public void ProcessMessage(IMessage[] messages)
+        {
+            messages.ToList().ForEach(m => Console.WriteLine("handler1 processing - " + m.Text));
+        }
+    }
+}
